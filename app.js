8691eb656af97f562aad2f7ceb437b206a06c433
Anthony <ajclay7@gmail.com>
2022-01-18 23:34:49 -0500

    clean up files

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
 const getNotes = require('./notes.js');
 
-getNotes();
+const msg = getNotes();
 
-// Challenge: Define and use a function in a new file
-//
-// 1. Create a new file called notes.js
-// 2. Create getNotes function that returns "Your notes..."
-// 3. Export getNotes function
-// 4. From app.js load in and call the function printing message to console
-
-// const add = require('./utils.js');
-// const sum = add(4, -2);
-// console.log(sum);
+console.log(msg);
--- notes.js
+++ notes.js
@@ -1,5 +1,5 @@
 const getNotes = function () {
-	console.log('Your notes...');
+	return 'Your notes...';
 };
 
 module.exports = getNotes;
